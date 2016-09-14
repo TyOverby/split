fd35fe9bfa28341111f20a3d82f2813fc4ca2c28
Ty Overby <ty@pre-alpha.com>
2016-09-14 10:33:06 -0700

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::io::{BufReader, Read, Write, copy, BufWriter};
+use std::io::{BufReader, Read, copy, BufWriter};
 use std::fs::{File, metadata};
 use std::io::Result as IoResult;
 use std::path::Path;
@@ -11,26 +11,20 @@ struct TruncRead<R: Read> {
 
 impl <R> Read for TruncRead<R> where R: Read {
     fn read(&mut self, buf: &mut [u8]) -> IoResult<usize> {
-        if self.count >= self.max {
-            Ok(0)
+        let read = if self.count >= self.max {
+            0
         } else if buf.len() + self.count > self.max {
             let actual_len = self.max - self.count;
-            let r = self.inner.read(&mut buf[..actual_len]);
-
-            if let Ok(bytes_read) = r {
-                self.count += bytes_read;
-            }
-
+            let r = try!(self.inner.read(&mut buf[..actual_len]));
+            self.count += r;
             r
         } else {
-            let r = self.inner.read(buf);
-
-            if let Ok(bytes_read) = r {
-                self.count += bytes_read;
-            }
-
+            let r = try!(self.inner.read(buf));
+            self.count += r;
             r
-        }
+        };
+
+        Ok(read)
     }
 }
 
@@ -52,13 +46,14 @@ fn split_file<P1, P2, P3>(input: P1, out1: P2, out2: P3) -> IoResult<()>
 where P1: AsRef<Path>, P2: AsRef<Path>, P3: AsRef<Path> {
     let input_size = try!(metadata(&input)).len();
     let first_file_size = input_size / 2;
-    println!("first of size {}", first_file_size);
-    let mut input_buffer = BufReader::new(try!(File::open(input)));
+
+    let input_buffer = BufReader::new(try!(File::open(input)));
     let mut first_out = BufWriter::new(try!(File::create(out1)));
     let mut second_out = BufWriter::new(try!(File::create(out2)));
 
     let mut first_reader = TruncRead::new(input_buffer, first_file_size as usize);
     try!(copy(&mut first_reader, &mut first_out));
+
     let mut second_reader = first_reader.unwrap();
     try!(copy(&mut second_reader, &mut second_out));
 
